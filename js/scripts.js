17d196de0850c1960d68225d9800af6fc2478975
Cheshire Cat <thacheshirecat@gmail.com>
2018-06-04 19:32:22 -0700

    Add initial HTML, JS and CSS needed for the word play excersize, and complete most of the JS function needed to complete the task.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+$("#sentance-form").submit(function(event) {
+  event.preventDefault();
+
+  var sentanceInput = $("#sentance").val();
+  var words = sentanceInput.split(" ");
+  var newWords = [];
+
+  words.forEach(function(word){
+    var wordLength = word.length;
+    if (wordLength >= 4) {
+      newWords.push(word);
+    }
+  });
+
+  newWords.toString(" ");
+  $(".simple").text(newWords);
+  $("#sentance-scramble").show();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
